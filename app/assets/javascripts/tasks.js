f27eab28d66cd84a2a90d94306b7beb3eb72c452
Knoll <noll.kyle@gmail.com>
2018-07-15 16:14:44 -0500

    Fix bug where remove button won't work after toggle

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -33,7 +33,7 @@
         var liHtml = taskHtml(data);
         var $li = $("#listItem-" + data.id);
         $li.replaceWith(liHtml);
-        $('.toggle').change(toggleTask);
+        updateHtml();
       });
     }
 
